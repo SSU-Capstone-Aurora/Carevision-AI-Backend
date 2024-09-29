7979e8dcc61b494837e99d0b1ec3bdcbee755564
88dldl <chltnrgml0999@gmail.com>
2024-09-30 02:43:13 +0900

    [refactor] json형식의 리턴을 화면에 보이도록 수정

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/ai-health', methods=['GET'])
 def health_check():
-    return jsonify(status='healthy', message='Im healthy!!'), 200
+    status = "healthy"
+    message = "I'm healthy!!"
+    return render_template('health_check.html', status=status, message=message)
 
 if __name__ == '__main__':
     app.run()
