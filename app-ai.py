f084abd1c0f2168c0042bddc6d0d62c846099f28
88dldl <chltnrgml0999@gmail.com>
2024-10-23 18:31:08 +0900

    feat : 유저 영상 스트리밍 API 추가

--- app-ai.py
+++ app-ai.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 
+from camera_stream.stream_sendto_kafka import stream_rtsp_and_send_to_kafka
+from test.api_test import router
+
 app = FastAPI()
+app.include_router(router)
 
 @app.get('/ai-health')
 def health_check():
     return {"I'm healthy!!!"}
+
+@app.get("/video/{user_id}")
+def stream_video(topic: str, user_id: str):
+    return stream_rtsp_and_send_to_kafka(topic, user_id) #병원을 토픽으로, 환자 key, data값으로 저장
