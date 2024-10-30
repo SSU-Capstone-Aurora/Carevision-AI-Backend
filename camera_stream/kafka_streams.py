5e9278652021f2d8434789b302817e483e87d4c9
88dldl <chltnrgml0999@gmail.com>
2024-10-30 15:20:28 +0900

    refactor: send_to_topic 입력 매개변수 변경

--- camera_stream/kafka_streams.py
+++ camera_stream/kafka_streams.py
@@ -16,15 +16,16 @@ async def connect_broker():
 def subscribe_to_topic(topic_name):
     @broker.subscriber(topic_name)
     async def handle_video(msg):
-        #내부 처리 로직 필요
-        print("handle_video" + msg)
+        print("subscriber 동작 중...")
+
+        await send_to_topic("tmp_topic", msg)
+        print("subscriber 동작 완료")
+
 
 # Kafka로 메시지 전송
-async def send_to_topic(topic_name):
+async def send_to_topic(topic_name, msg):
     @broker.publisher(topic_name)
-    async def send_processed_result(result):
-        print(f"send_processed_result 실행됨, 메시지: {result}")  # 로그 추가
-        await broker.publish(result, topic_name)
-        print(f"Kafka에 {topic_name}로 메시지 전송 완료: {result}")  # 메시지 전송 완료 로그
+    async def send_processed_result(msg):
+        print(f"Kafka에 {topic_name}로 메시지 전송 완료: {msg}")
 
-    return send_processed_result  # 비동기 함수 반환
+    await send_processed_result(msg)
