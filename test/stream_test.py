a1ca21b2974952ce84316a0f085247e881606e20
88dldl <chltnrgml0999@gmail.com>
2024-10-30 15:20:48 +0900

    remove: 테스트에 필요한 코드만 남도록 수정

--- test/stream_test.py
+++ test/stream_test.py
@@ -1,19 +1,6 @@
-import os
+from config.kafka_broker_instance import broker
 
-from faststream.kafka import KafkaBroker
-
-from camera_stream.kafka_streams import send_to_topic, startup_event
-
-# Kafka 브로커 설정
-broker_env = os.environ.get("BROKER")
-broker = KafkaBroker(broker_env)
 
 async def send_example():
-    await startup_event()
-
-    print("send_example 함수 호출")
-
-    send_processed_result = await send_to_topic("test")
-    await send_processed_result("hello")
-
-    print("send_to_topic 함수 실행 완료")
+    await broker.publish(message="test", topic="tmp_topic")
+    print("tmp_topic에 publish 완료")
