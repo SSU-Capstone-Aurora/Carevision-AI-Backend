03667b4008d7aec7f028994fcb90f1aea5f740e3
88dldl <chltnrgml0999@gmail.com>
2024-10-30 15:14:43 +0900

    chore: 테스트 종료시 출력되는 로그 추가

--- test/api_test.py
+++ test/api_test.py
@@ -5,5 +5,7 @@ from test.stream_test import send_example
 router = APIRouter()
 
 @router.get('/test')
-async def test_send():  # 이 함수도 비동기 함수로 변경
-    await send_example()  # await를 사용하여 비동기 호출
+async def test_send():
+    await send_example()
+    print("send_example 함수 실행 완료")
+
