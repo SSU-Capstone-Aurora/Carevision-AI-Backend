63216ad12795c004b8e4266f4b9efe3fb855f21d
88dldl <chltnrgml0999@gmail.com>
2024-10-30 15:16:23 +0900

    feat: startup 이벤트 발생시 브로커 연결 및 FastStream 실행

--- app-ai.py
+++ app-ai.py
@@ -1,10 +1,21 @@
+import asyncio
+
 from fastapi import FastAPI
 
+from camera_stream.kafka_streams import connect_broker
 from camera_stream.stream_sendto_kafka import stream_rtsp_and_send_to_kafka
+from config.kafka_broker_instance import kafka_app
 from test.api_test import router
 
 app = FastAPI()
 app.include_router(router)
+
+@app.on_event("startup")
+async def startup_event():
+    await connect_broker()
+
+    # FastStream 애플리케이션 실행
+    asyncio.create_task(kafka_app.run())
 
 @app.get('/ai-health')
 def health_check():
