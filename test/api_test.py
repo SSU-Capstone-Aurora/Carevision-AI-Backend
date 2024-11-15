01f21181b91ccfe36aae2a02154cdeebc949ae98
88dldl <chltnrgml0999@gmail.com>
2024-11-15 12:57:38 +0900

    feat: ffmpeg로 실시간 영상 받아오고 내보내는 테스트 코드 작성

--- test/api_test.py
+++ test/api_test.py
@@ -1,11 +1,18 @@
 from fastapi import APIRouter
 
+from test.ffmpeg_test import ffmpeg_test
 from test.stream_test import send_example
 
 router = APIRouter()
+
 
 @router.get('/test')
 async def test_send():
     await send_example()
     print("send_example 함수 실행 완료")
 
+
+@router.get('/test/stream')
+def test_ffmpeg():
+    print("ffmpeg 테스트 실행")
+    ffmpeg_test()
