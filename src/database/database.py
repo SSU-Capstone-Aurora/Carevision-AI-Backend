5bc1ac8f946a3423c00453a62fccfc6d66a1db2c
88dldl <chltnrgml0999@gmail.com>
2024-11-16 18:20:55 +0900

    refactor: 데이터베이스 호스트 환경변수화

--- src/database/database.py
+++ src/database/database.py
@@ -9,7 +9,7 @@ from src.error import camera_error
 load_dotenv()
 
 # 환경 변수에서 데이터베이스 설정 가져오기
-host = "host.docker.internal"
+host = os.getenv('DB_HOST')
 user = os.getenv('DB_USER')
 password = os.getenv('DB_PASSWORD')
 database = os.getenv('DB_NAME')
