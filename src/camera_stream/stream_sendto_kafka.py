cc134f7d77d4da6a153a7056fe217f067e75b562
CWPARK <leopark9917@gmail.com>
2024-11-17 23:10:15 +0900

    [FEAT] kafka gcm 고정갑 암볷호화 추가

--- src/camera_stream/stream_sendto_kafka.py
+++ src/camera_stream/stream_sendto_kafka.py
@@ -2,7 +2,7 @@ import os
 import zlib
 import cv2
 from dotenv import load_dotenv
-
+from Crypto.Cipher import AES
 from src.config.kafka_broker_instance import broker
 
 # .env 파일 로딩
@@ -12,10 +12,17 @@ camera_pw = os.environ.get("CAMERA_PASSWORD")
 camera_ip = os.environ.get("CAMERA_IP")
 url = f"rtsp://{camera_id}:{camera_pw}@{camera_ip}/cam/realmonitor?channel=1&subtype=0"
 
-# FastStream, Kafka 브로커 설정
-broker_env = os.environ.get("BROKER")
+# AES 암호화에 사용할 키와 IV 로딩
+aes_key = os.environ.get("AES_KEY").encode()  # 32바이트 키 (256비트)
+aes_iv = os.environ.get("AES_IV").encode()    # 16바이트 IV (nonce)
 
-# 병원을 토픽으로, 환자 key, data값으로 저장
+# AES GCM 암호화 함수
+def encrypt_data_gcm(data):
+    cipher = AES.new(aes_key, AES.MODE_GCM, nonce=aes_iv)
+    ciphertext, tag = cipher.encrypt_and_digest(data)
+    return ciphertext, tag
+
+# 병원을 토픽으로, 환자 key, data 값으로 저장
 async def stream_rtsp_and_send_to_kafka(kafka_topic, user_id):
     frame_counter = 0
     frame_interval = 270  # 10초
@@ -37,7 +44,7 @@ async def stream_rtsp_and_send_to_kafka(kafka_topic, user_id):
         # 프레임 카운터 증가
         frame_counter += 1
 
-        # 설정 프레임마다 분석 작업 실행
+        # 설정된 프레임마다 분석 작업 실행
         if frame_counter % frame_interval == 0:
             ret, buffer = cv2.imencode('.jpg', frame)
             if not ret:
@@ -47,8 +54,11 @@ async def stream_rtsp_and_send_to_kafka(kafka_topic, user_id):
             # JPEG 데이터를 zlib로 압축
             compressed_frame = zlib.compress(buffer)
 
-            # 프레임을 Kafka 서버로 전송
-            await broker.publish(compressed_frame, kafka_topic, key=user_id)
+            # 압축된 프레임을 AES GCM 암호화
+            encrypted_frame, tag = encrypt_data_gcm(compressed_frame)
+
+            # Kafka로 암호화된 데이터 전송 (nonce와 tag 포함)
+            await broker.publish(encrypted_frame + aes_iv + tag, kafka_topic, key=user_id)
 
     # 모든 작업 종료 후 자원 해제
     cap.release()
